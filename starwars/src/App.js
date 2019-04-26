5ac4cfbe5e1d0904246d23db2aaa6dab4802f5b2
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-26 16:11:19 -0400

    List Styling (MVP)
    - Flex boxing
    - Margin 
    - Background color
    - Height

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -32,10 +32,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header" font = "Roboto">React Wars</h1>
         {this.state.starwarsChars.map(char => {
           return (
-            <li className = "girl">{char.name}</li>
+            <li className = "starwarsppl">{char.name}</li>
           )
         })}
       </div>
